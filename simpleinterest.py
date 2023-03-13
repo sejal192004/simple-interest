b07c35d12744f11590fe42b94d688bfecdfddeb9
sejal192004 <127733566+sejal192004@users.noreply.github.com>
2023-03-13 14:56:50 +0530

    Create simpleinterest.py

---
+++ simpleinterest.py
@@ -0,0 +1,6 @@
+principal float(input("Enter the principal: "))
+rate = float(input("Enter the rate of interest: "))
+time = float(input("Enter the time: "))
+
+si = (principal * time * rate) / 100
+print("The simple interest is Rs",si);
